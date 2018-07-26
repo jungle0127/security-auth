f8cca9731f5a960d47507796267be9c064d49a02
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2018-07-26 13:11:26 +0800

    initial project

---
+++ security-auth-app/src/main/java/com/ps/security/auth/app/App.java
@@ -0,0 +1,13 @@
+package com.ps.security.auth.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ security-auth-client/src/main/java/com/ps/security/auth/client/App.java
@@ -0,0 +1,13 @@
+package com.ps.security.auth.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
