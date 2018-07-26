5d8910de4202841066aeb87db0a6bd02cf54bb46
jungle0127 <arbor@foxmail.com>
2018-07-26 23:17:16 +0800

    updated pom

---
+++ security-auth-client/src/main/java/com/ps/security/auth/client/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.ps.security.auth.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Security";
+	}
+}
