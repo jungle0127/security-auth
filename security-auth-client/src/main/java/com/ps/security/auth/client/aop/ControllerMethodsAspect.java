d6755be0c100fc1f6db4f7867e28678d4d39f1b0
jungle0127 <arbor@foxmail.com>
2018-07-27 21:58:31 +0800

    added aop for logging demo

--- security-auth-client/src/main/java/com/ps/security/auth/client/aop/ControllerMethodsAspect.java
+++ security-auth-client/src/main/java/com/ps/security/auth/client/aop/ControllerMethodsAspect.java
@@ -1,8 +1,28 @@
 package com.ps.security.auth.client.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class ControllerMethodsAspect {
-	
+	private static final Logger logger = LoggerFactory.getLogger(ControllerMethodsAspect.class);
+	@Pointcut("within(com.ps.security.auth.client..controller.*)")
+	public void controllerMethod() {
+		
+	}
+	@Before("controllerMethod()")
+	public void logBefore(JoinPoint joinPoint) {
+		logger.info("Before the method");
+	}
+	@After("controllerMethod()")
+	public void logAfter(JoinPoint joinPoint) {
+		logger.info("After the method");
+	}
 }
